42f6b782d180c33a965b84f81b8bb69b50027c90
dzhdan <dzhdan@nvidia.com>
2025-03-06 16:35:09 +0800

    Improved "float4_to[s/u]norm" and "[s/u]norm_to_float4": - improved robustness - suitable "const" replaced with "constexpr" to highlight "compile time" math - added support for 0 bits for some channels

--- Guts/packing.h
+++ Guts/packing.h
@@ -16,14 +16,14 @@ template <uint32_t Rbits, uint32_t Gbits, uint32_t Bbits, uint32_t Abits>
 ML_INLINE uint32_t float4_to_unorm(const float4& v) {
     ML_StaticAssertMsg(Rbits + Gbits + Bbits + Abits <= 32, "Sum of all bit must be <= 32");
 
-    const uint32_t Rmask = (1 << Rbits) - 1;
-    const uint32_t Gmask = (1 << Gbits) - 1;
-    const uint32_t Bmask = (1 << Bbits) - 1;
-    const uint32_t Amask = (1 << Abits) - 1;
+    constexpr uint32_t Rmask = (1 << Rbits) - 1;
+    constexpr uint32_t Gmask = (1 << Gbits) - 1;
+    constexpr uint32_t Bmask = (1 << Bbits) - 1;
+    constexpr uint32_t Amask = (1 << Abits) - 1;
 
-    const uint32_t Gshift = Rbits;
-    const uint32_t Bshift = Gshift + Gbits;
-    const uint32_t Ashift = Bshift + Bbits;
+    constexpr uint32_t Gshift = Rbits & 31;
+    constexpr uint32_t Bshift = (Gshift + Gbits) & 31;
+    constexpr uint32_t Ashift = (Bshift + Bbits) & 31;
 
     const v4f scale = v4f_set(float(Rmask), float(Gmask), float(Bmask), float(Amask));
 
@@ -31,9 +31,9 @@ ML_INLINE uint32_t float4_to_unorm(const float4& v) {
     v4i i = _mm_cvtps_epi32(t);
 
     uint32_t p = _mm_cvtsi128_si32(i);
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(1, 1, 1, 1))) << Gshift;
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(2, 2, 2, 2))) << Bshift;
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(3, 3, 3, 3))) << Ashift;
+    p |= Gbits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(1, 1, 1, 1))) << Gshift);
+    p |= Bbits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(2, 2, 2, 2))) << Bshift);
+    p |= Abits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(3, 3, 3, 3))) << Ashift);
 
     return p;
 }
@@ -59,8 +59,6 @@ ML_INLINE uint32_t float2_to_unorm_16_16(const float2& v) {
 }
 
 ML_INLINE uint32_t float3_to_ufloat_11_11_10(const float3& v) {
-    ML_Assert(v.x >= 0.0f && v.y >= 0.0f && v.z >= 0.0f);
-
     uint32_t r = ToSmallFloat<UF11_M_BITS, UF11_E_BITS, UF11_S_MASK>(v.x);
     r |= ToSmallFloat<UF11_M_BITS, UF11_E_BITS, UF11_S_MASK>(v.y) << 11;
     r |= ToSmallFloat<UF10_M_BITS, UF10_E_BITS, UF10_S_MASK>(v.z) << 22;
@@ -72,21 +70,21 @@ template <uint32_t Rbits, uint32_t Gbits, uint32_t Bbits, uint32_t Abits>
 ML_INLINE uint32_t float4_to_snorm(const float4& v) {
     ML_StaticAssertMsg(Rbits + Gbits + Bbits + Abits <= 32, "Sum of all bit must be <= 32");
 
-    const uint32_t Rmask = (1 << Rbits) - 1;
-    const uint32_t Gmask = (1 << Gbits) - 1;
-    const uint32_t Bmask = (1 << Bbits) - 1;
-    const uint32_t Amask = (1 << Abits) - 1;
+    constexpr uint32_t Rmask = (1 << Rbits) - 1;
+    constexpr uint32_t Gmask = (1 << Gbits) - 1;
+    constexpr uint32_t Bmask = (1 << Bbits) - 1;
+    constexpr uint32_t Amask = (1 << Abits) - 1;
 
-    const uint32_t Gshift = Rbits;
-    const uint32_t Bshift = Gshift + Gbits;
-    const uint32_t Ashift = Bshift + Bbits;
+    constexpr uint32_t Gshift = Rbits & 31;
+    constexpr uint32_t Bshift = (Gshift + Gbits) & 31;
+    constexpr uint32_t Ashift = (Bshift + Bbits) & 31;
 
-    const uint32_t Rrange = (1 << (Rbits - 1)) - 1;
-    const uint32_t Grange = (1 << (Gbits - 1)) - 1;
-    const uint32_t Brange = (1 << (Bbits - 1)) - 1;
-    const uint32_t Arange = (1 << (Abits - 1)) - 1;
+    constexpr float Rrange = (1 << (Rbits == 0 ? 1 : (Rbits - 1))) - 1;
+    constexpr float Grange = (1 << (Gbits == 0 ? 1 : (Gbits - 1))) - 1;
+    constexpr float Brange = (1 << (Bbits == 0 ? 1 : (Bbits - 1))) - 1;
+    constexpr float Arange = (1 << (Abits == 0 ? 1 : (Abits - 1))) - 1;
 
-    const v4f scale = v4f_set(float(Rrange), float(Grange), float(Brange), float(Arange));
+    const v4f scale = v4f_set(Rrange, Grange, Brange, Arange);
     const v4i mask = _mm_setr_epi32(Rmask, Gmask, Bmask, Amask);
 
     v4f t = _mm_mul_ps(v.xmm, scale);
@@ -94,9 +92,9 @@ ML_INLINE uint32_t float4_to_snorm(const float4& v) {
     i = _mm_and_si128(i, mask);
 
     uint32_t p = _mm_cvtsi128_si32(i);
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(1, 1, 1, 1))) << Gshift;
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(2, 2, 2, 2))) << Bshift;
-    p |= _mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(3, 3, 3, 3))) << Ashift;
+    p |= Gbits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(1, 1, 1, 1))) << Gshift);
+    p |= Bbits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(2, 2, 2, 2))) << Bshift);
+    p |= Abits == 0 ? 0 : (_mm_cvtsi128_si32(_mm_shuffle_epi32(i, _MM_SHUFFLE(3, 3, 3, 3))) << Ashift);
 
     return p;
 }
@@ -156,16 +154,21 @@ template <uint32_t Rbits, uint32_t Gbits, uint32_t Bbits, uint32_t Abits>
 ML_INLINE float4 unorm_to_float4(uint32_t p) {
     ML_StaticAssertMsg(Rbits + Gbits + Bbits + Abits <= 32, "Sum of all bit must be <= 32");
 
-    const uint32_t Rmask = (1 << Rbits) - 1;
-    const uint32_t Gmask = (1 << Gbits) - 1;
-    const uint32_t Bmask = (1 << Bbits) - 1;
-    const uint32_t Amask = (1 << Abits) - 1;
+    constexpr uint32_t Rmask = (1 << Rbits) - 1;
+    constexpr uint32_t Gmask = (1 << Gbits) - 1;
+    constexpr uint32_t Bmask = (1 << Bbits) - 1;
+    constexpr uint32_t Amask = (1 << Abits) - 1;
 
-    const uint32_t Gshift = Rbits;
-    const uint32_t Bshift = Gshift + Gbits;
-    const uint32_t Ashift = Bshift + Bbits;
+    constexpr uint32_t Gshift = Rbits & 31;
+    constexpr uint32_t Bshift = (Gshift + Gbits) & 31;
+    constexpr uint32_t Ashift = (Bshift + Bbits) & 31;
 
-    const v4f scale = v4f_set(1.0f / Rmask, 1.0f / Gmask, 1.0f / Bmask, 1.0f / Amask);
+    constexpr float invRmask = Rmask == 0 ? 1.0f : 1.0f / Rmask;
+    constexpr float invGmask = Gmask == 0 ? 1.0f : 1.0f / Gmask;
+    constexpr float invBmask = Bmask == 0 ? 1.0f : 1.0f / Bmask;
+    constexpr float invAmask = Amask == 0 ? 1.0f : 1.0f / Amask;
+
+    const v4f scale = v4f_set(invRmask, invGmask, invBmask, invAmask);
 
     v4i i = _mm_setr_epi32(p & Rmask, (p >> Gshift) & Gmask, (p >> Bshift) & Bmask, (p >> Ashift) & Amask);
     v4f t = _mm_cvtepi32_ps(i);
@@ -201,23 +204,28 @@ template <uint32_t Rbits, uint32_t Gbits, uint32_t Bbits, uint32_t Abits>
 ML_INLINE float4 snorm_to_float4(uint32_t p) {
     ML_StaticAssertMsg(Rbits + Gbits + Bbits + Abits <= 32, "Sum of all bit must be <= 32");
 
-    const uint32_t Rmask = (1 << Rbits) - 1;
-    const uint32_t Gmask = (1 << Gbits) - 1;
-    const uint32_t Bmask = (1 << Bbits) - 1;
-    const uint32_t Amask = (1 << Abits) - 1;
+    constexpr uint32_t Rmask = (1 << Rbits) - 1;
+    constexpr uint32_t Gmask = (1 << Gbits) - 1;
+    constexpr uint32_t Bmask = (1 << Bbits) - 1;
+    constexpr uint32_t Amask = (1 << Abits) - 1;
 
-    const uint32_t Gshift = Rbits;
-    const uint32_t Bshift = Gshift + Gbits;
-    const uint32_t Ashift = Bshift + Bbits;
+    constexpr uint32_t Gshift = Rbits & 31;
+    constexpr uint32_t Bshift = (Gshift + Gbits) & 31;
+    constexpr uint32_t Ashift = (Bshift + Bbits) & 31;
 
-    const uint32_t Rsign = (1 << (Rbits - 1));
-    const uint32_t Gsign = (1 << (Gbits - 1));
-    const uint32_t Bsign = (1 << (Bbits - 1));
-    const uint32_t Asign = (1 << (Abits - 1));
+    constexpr uint32_t Rsign = 1 << (Rbits == 0 ? 0 : (Rbits - 1));
+    constexpr uint32_t Gsign = 1 << (Gbits == 0 ? 0 : (Gbits - 1));
+    constexpr uint32_t Bsign = 1 << (Bbits == 0 ? 0 : (Bbits - 1));
+    constexpr uint32_t Asign = 1 << (Abits == 0 ? 0 : (Abits - 1));
+
+    constexpr float invRsignMinus1 = Rbits == 0 ? 1.0f : 1.0f / (Rsign - 1);
+    constexpr float invGsignMinus1 = Gbits == 0 ? 1.0f : 1.0f / (Gsign - 1);
+    constexpr float invBsignMinus1 = Bbits == 0 ? 1.0f : 1.0f / (Bsign - 1);
+    constexpr float invAsignMinus1 = Abits == 0 ? 1.0f : 1.0f / (Asign - 1);
 
     const v4i vsign = _mm_setr_epi32(Rsign, Gsign, Bsign, Asign);
     const v4i vor = _mm_setr_epi32(~(Rsign - 1), ~(Gsign - 1), ~(Bsign - 1), ~(Asign - 1));
-    const v4f vscale = v4f_set(1.0f / (Rsign - 1), 1.0f / (Gsign - 1), 1.0f / (Bsign - 1), 1.0f / (Asign - 1));
+    const v4f vscale = v4f_set(invRsignMinus1, invGsignMinus1, invBsignMinus1, invAsignMinus1);
 
     v4i i = _mm_setr_epi32(p & Rmask, (p >> Gshift) & Gmask, (p >> Bshift) & Bmask, (p >> Ashift) & Amask);
 
